05ea6d06cb01e61aae77ad19ef5a65bb767d6750
AggelosTse <aggu8914@gmail.com>
2026-04-05 19:07:20 +0300

    added rating difference column, sorted dataset

--- model.py
+++ model.py
@@ -4,5 +4,18 @@ import numpy as np
 
 chess = pd.read_csv('./data/games.csv')      #open csv file
 
-chess_clean = chess.drop(columns=["id","white_id","black_id","created_at","last_move_at","moves",])
+#remove not used columns
+chess_clean = chess.drop(columns=["id","white_id","black_id","created_at","last_move_at","moves","victory_status","turns","opening_name"])
+
+
+#add new column with the difference of 2 player's elo
+chess_clean["rating_diff"] = chess_clean["white_rating"] - chess_clean["black_rating"]
+
+
+#reorder important columns
+chess_clean = chess_clean[['white_rating', 'black_rating', 'rated','increment_code','opening_eco','opening_ply','rating_diff','winner']]
+
+
+
+
 print(chess_clean.head(10))
